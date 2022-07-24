ddc4d6620a305ec9408fc4594a6739dbba535884
Alkayzer <marceloperezbritos@gmail.com>
2022-07-24 13:51:59 -0300

    last changes

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -2,25 +2,20 @@ package db
 
 import (
 	"database/sql"
+	"github.com/Alkayzer/simplebank/util"
 	_ "github.com/lib/pq"
 	"log"
 	"os"
 	"testing"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-)
-
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
+	config, err := util.LoadConfig("../..")
 
-	var err error
-
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("Cannot connect to db", err)
